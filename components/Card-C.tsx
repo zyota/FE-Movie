ae191c30745c556a053f81f4ecae8091eb1fc0c2
Morning <morning@admins-iMac-1-5.local>
2023-04-06 12:16:43 +0800

    4/6 c

--- components/Card-C.tsx
+++ components/Card-C.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 type Movie = {
   title: string;
@@ -12,28 +13,31 @@ type Movie = {
 export default function Card(prop: Movie): JSX.Element {
   console.log(prop);
   return (
-    <div className="flex justify-center">
-      <div className="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700">
-        <a href="#!">
-          <img className="rounded-t-lg" src={prop.poster} alt="" />
-        </a>
-        <div className="p-6">
-          <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-            {prop.title}
-          </h5>
-          <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-            {/* {prop.fullplot} */}
-          </p>
-          <button
-            type="button"
-            className="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-            data-te-ripple-init
-            data-te-ripple-color="light"
-          >
-            {prop.year}
-          </button>
+    <div>
+      <Link href={`/movies/${prop._id}`}>
+        <div className="flex justify-center">
+          <div className="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+            <img className="rounded-t-lg" src={prop.poster} alt="" />
+
+            <div className="p-6">
+              <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                {prop.title}
+              </h5>
+              <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                {/* {prop.fullplot} */}
+              </p>
+              <button
+                type="button"
+                className="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+              >
+                {prop.year}
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -1,7 +1,40 @@
+import axios from "axios";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Movie() {
-  const [data, setData] = useState<MovieType | null>(null);
+type Movie = {
+  title: string;
+  year: number;
+  poster: string;
+  fullplot: string;
+  _id: string;
+  genres: Array<string>;
+};
+
+export default function Movie(): JSX.Element {
+  const [data, setData] = useState<Movie | null>(null);
   const { query } = useRouter();
+
+  useEffect(() => {
+    if (query.id) {
+      axios
+        .get(`http://localhost:4000/movies/${query.id}`)
+        // .then((res) => res.json())
+        .then((res) => setData(res.data));
+    }
+  }, [query.id]);
+  console.log("data: ", data);
+  return (
+    <div>
+      ID: {query.id}
+      <div>
+        <>
+          <h2>{data?.title}</h2>
+          <picture>
+            <img src={data?.poster} alt="" />
+          </picture>
+        </>
+      </div>
+    </div>
+  );
 }
